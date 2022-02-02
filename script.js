1c9df1831758d5fe720d3fe4a991b1c05311d8de
Pavanreddy Bojjireddy <pavanreddy0920@gmail.com>
2022-02-02 20:54:55 +1100

    date functions extra

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ const account1 = {
     '2020-01-28T09:15:04.904Z',
     '2020-04-01T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2022-02-01T17:01:17.194Z',
+    '2022-01-30T23:36:17.929Z',
+    '2022-01-31T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -165,6 +165,13 @@ createUsernames(accounts);
 ///////////////////////////////////////////////////////////////////////////////
 
 const getDateWithTime = function (date, withTime = true) {
+  const calculateDays = Math.round((new Date() - date) / (1000 * 60 * 60 * 24));
+  console.log(calculateDays);
+
+  if (calculateDays === 0) return 'Today';
+  if (calculateDays === 1) return 'Yesterday';
+  if (calculateDays <= 7) return `${calculateDays} days ago`;
+
   const day = `${date.getDate()}`.padStart(2, 0);
   const month = `${date.getMonth() + 1}`.padStart(2, 0);
   const year = date.getFullYear();
